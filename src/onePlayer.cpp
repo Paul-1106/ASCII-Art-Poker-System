e985c19892f5eb0f7c95112bd61f7bf6949a1fd5
Paul-1106 <pharriott116@gmail.com>
2026-04-17 10:19:09 -0400

    Implementing onePlayer.cpp page

--- src/onePlayer.cpp
+++ src/onePlayer.cpp
@@ -15,17 +15,17 @@ void Poker::onePlayer(size_t& credit, size_t creditRequirement) {
     std::string card1, card2, card3, card4, card5;
     std::vector<std::string> pokerHand = {card1, card2, card3, card4, card5};
     char swap = ' ';
-    // const std::vector<std::string>& cardDeck = {twoH, twoD, twoC, twoS, twoQ, threeH, threeD, threeC, threeS, threeQ, fourH, fourD, fourC, fourS, fourQ, fiveH,
-    // fiveD, fiveC, fiveS, fiveQ, sixH, sixD, sixC, sixS, sixQ, sevenH, sevenD, sevenC, sevenS, sevenQ, eightH, eightD, eightC, eightS, eightQ, nineH, nineD, nineC,
-    // nineS, nineQ, tenH, tenD, tenC, tenS, tenQ, jackH, jackD, jackC, jackS, jackQ, queenH, queenD, queenC, queenS, queenQ, kingH, kingD, kingC, kingS, kingQ,
-    // aceH, aceD, aceC, aceS, aceQ};
+    const std::vector<std::string>& cardDeck = {twoH, twoD, twoC, twoS, twoQ, threeH, threeD, threeC, threeS, threeQ, fourH, fourD, fourC, fourS, fourQ, fiveH,
+    fiveD, fiveC, fiveS, fiveQ, sixH, sixD, sixC, sixS, sixQ, sevenH, sevenD, sevenC, sevenS, sevenQ, eightH, eightD, eightC, eightS, eightQ, nineH, nineD, nineC,
+    nineS, nineQ, tenH, tenD, tenC, tenS, tenQ, jackH, jackD, jackC, jackS, jackQ, queenH, queenD, queenC, queenS, queenQ, kingH, kingD, kingC, kingS, kingQ,
+    aceH, aceD, aceC, aceS, aceQ};
     
     
     // The reqirement of playing one player mode of poker is to have at least 100 credits in credit balance
-    while (credit >= creditRequirement) {
+    while (credit >= creditRequirement && turn < 2) {
 
         // 'turn' variable will add up by 1 when user decides to swap some cards, else the variable will add up by 2
-        if (credit >= creditRequirement && turn < 2) {
+        if (credit >= creditRequirement) {
             
             // Your credit will automatically deduct when betting the amount of credits on poker
             std::cout << "Enter your bet: \n";
@@ -48,21 +48,21 @@ void Poker::onePlayer(size_t& credit, size_t creditRequirement) {
                     std::string randomCard = pokerHand[dist(mEngine)];
 
                     // CONTINUE
-                    card1;
-                    card2;
-                    card3;
-                    card4;
-                    card5;
 
-                    std::cout << randomCard << randomCard << randomCard << randomCard << randomCard << "\n\n";
+                    std::cout << twoH << randomCard << randomCard << randomCard << randomCard << "\n\n";
+                    turn += 1;
                     std::cout << "Swap cards or pass? (s = Swap | p = Pass)\n";
                     std::cin >> swap;
 
                     if (swap == 's') {
 
+                        std::cout << "Select cards you want to replace.\n";
+                        //std::cin 
+                        turn += 1;
                     }
                     else if (swap == 'p') {
 
+                        turn += 1;
                     }
                     else {
                         std::cout << "That input is not valid. Please select 's' to swap your deck or 'p' if you are satisfy with your deck.\n";
